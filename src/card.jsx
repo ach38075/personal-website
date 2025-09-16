3a7d16a8ba8a6c435e1f8b243774a9c29e82299d
ach38075 <ach38075@uga.edu>
2025-09-16 14:07:39 -0400

    first iteration of website

--- src/card.jsx
+++ src/card.jsx
@@ -3,11 +3,14 @@ import "./index.css";
 export default function Card() {
   return (
     <>
-      <div className="flex flex-row bg-emerald-500 rounded-2xl p-6">
-        <img src="/avatar.jpg" className="h-72 w-72 rounded-full" />
-        <div className="flex flex-col items-centered justify-start p-10 m-4">
+      <div className="flex flex-row bg-rose-200 rounded-2xl p-6">
+        <img src="/headshot.jpg" className="h-72 w-72 rounded-full ring-6 ring-rose-400" />
+        <div className="flex flex-col items-centered justify-start p-10 m-4" style={{ color: "#fb7185" }}>
             <h1 className="text-6xl">Audrey Holbrook</h1>
-            <p className="text-xl">3rd year undergrad Computer Science student at UGA</p>
+            <p className="text-xl">4th year undergrad Computer Science student at the University of Georgia</p>
+            <p className="text-l italic">with a concentration in Computer Systems</p>
+            <a href= "contact.html" className="text-xl pt-3 underline hover:text-rose-600">Contact me! </a> 
+            <a href= "resume.html" className="text-xl underline hover:text-rose-600">Check out my resume! </a> 
         </div>
       </div>
     </>
